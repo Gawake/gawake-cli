b1fdd9391ab078db38c30d892f3d7cf722297b69
Kelvin Novais <kelvin.novais@protonmail.com>
2023-08-30 17:50:22 -0300

    Revert  makefile variable; fixed makefile install on Debian

--- cli/install.sh
+++ cli/install.sh
@@ -4,27 +4,30 @@ err_counter=0
 
 echo "Installing gawake-cli..."
 
-# Install the main binary to /bin
+# [1] Install the main binary to /bin
 echo "Installing the main binary to /bin"
 install -v ./build/gawake-cli /usr/bin/                                                       # src destination
 err_counter=$(($? + $err_counter))
 
-# Install the helper binaries to /opt/gawake/bin/cli
+# [2] Install the helper binaries to /opt/gawake/bin/cli
 echo "Installing the helper binaries to /opt/gawake/bin/cli"
 install -v -o root -g root -m 754 -D -t /opt/gawake/bin/cli/ ./build/{cron*,db_checker}       # destination src
 err_counter=$(($? + $err_counter))
 
-# Install the cron service to /etc/cron.d
+# [3] Install the uninstall script
+install -v -o root -g root -m 555 uninstall.sh /opt/gawake/                                   # src destination
+err_counter=$(($? + $err_counter))
+
+# [4] Install the cron service to /etc/cron.d
 echo "Installing the cron service to /etc/cron.d"
 install -v -o root -g root -m 644 gawake /etc/cron.d/                                         # src destination
 err_counter=$(($? + $err_counter))
 
-# Adding Gawake user
+# [5] Adding Gawake user
 useradd -Mr gawake
 
-# Install uninstall script
-install -v -o root -g root -m 555 uninstall.sh /opt/gawake/                                   # src destination
-err_counter=$(($? + $err_counter))
+# [6] Creating logs directory
+mkdir -p /var/gawake/logs/
 
 if [ $err_counter -eq 0 ]; then
   echo "Gawake installed successfully!"

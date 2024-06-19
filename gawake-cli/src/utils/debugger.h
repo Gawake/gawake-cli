8140af6e9971dfeac2b974cf6040909429a0c9a9
Kelvin Novais <kelvin.novais@protonmail.com>
2024-06-18 23:52:05 -0300

    Improving debugger function

--- gawake-cli/src/utils/debugger.h
+++ gawake-cli/src/utils/debugger.h
@@ -6,28 +6,40 @@
 const char *print_timestamp (void);
 
 /*
- * Prints "** DEBUG" magenta colored;
- * then the file, line and function;
- * finally the message passed to the macro
- *
  * USAGE: DEBUG_PRINT (("Var 1: %d, string: %s", var1, str))
  * Outputs to stdout, just for developing debugging reasons
  */
-#if PREPROCESSOR_DEBUG
+#if PREPROCESSOR_DEBUG == 1
+#   define DEBUG_PRINT(x) printf ("\x1b[35mDEBUG:\x1b[0m "); \
+                                printf x; printf ("\n\n"); fflush (stdout);
+
+#   define DEBUG_PRINT_TIME(x) printf ("\x1b[35mDEBUG:\x1b[0m "); \
+                             printf x; printf ("\n\n"); fflush (stdout);
+
+#elif PREPROCESSOR_DEBUG == 2
+#   define DEBUG_PRINT(x) printf ("\x1b[35mDEBUG:\x1b[0m %s(): ", __func__); \
+                                printf x; printf ("\n\n"); fflush (stdout);
+
+#   define DEBUG_PRINT_TIME(x) printf ("\x1b[35mDEBUG:\x1b[0m %s() at %s: ", \
+                             __func__, print_timestamp ()); \
+                             printf x; printf ("\n\n"); fflush (stdout);
+
+#elif PREPROCESSOR_DEBUG == 3
 # define DEBUG_PRINT(x) printf ("\x1b[35mDEBUG:\x1b[0m %s:%d:%s(): ", \
                                 __FILE__, __LINE__, __func__); \
                                 printf x; printf ("\n\n"); fflush (stdout);
-#else
-# define DEBUG_PRINT(x) /* Don't do anything in release builds */
-#endif
 
-/* Similar to previous macro, but also prints the time */
-#if PREPROCESSOR_DEBUG
-# define DEBUG_PRINT_TIME(x) printf ("\x1b[35mDEBUG:\x1b[0m %s:%d:%s() at %s: ", \
+#   define DEBUG_PRINT_TIME(x) printf ("\x1b[35mDEBUG:\x1b[0m %s:%d:%s() at %s: ", \
                              __FILE__, __LINE__, __func__, print_timestamp ()); \
                              printf x; printf ("\n\n"); fflush (stdout);
+
 #else
-# define DEBUG_PRINT_TIME(x) /* Don't do anything in release builds */
+/*
+ * PREPROCESSOR_DEBUG == 0
+ * Don't do anything in release builds
+ */
+#   define DEBUG_PRINT(x)
+#   define DEBUG_PRINT_TIME(x)
 #endif
 
 // Independent of PREPROCESSOR_DEBUG:

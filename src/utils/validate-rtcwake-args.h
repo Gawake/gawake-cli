8965f576d0a31b7ef7ecab992de4c9bb43677a17
Kelvin Novais <kelvin.novais@protonmail.com>
2024-12-24 14:29:15 -0300

    remove old code

--- src/utils/validate-rtcwake-args.h
+++
@@ -1,8 +0,0 @@
-#ifndef VALIDATE_RTCWAKE_ARGS_H_
-#define VALIDATE_RTCWAKE_ARGS_H_
-
-#include "gawake-types.h"
-
-int validade_rtcwake_args (RtcwakeArgs *rtcwake_args);
-
-#endif /* VALIDATE_RTCWAKE_ARGS_H_ */
